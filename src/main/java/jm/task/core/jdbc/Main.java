12b7de481218bb1e2fb51bd7b4aba3184f0d88f7
Noteburg <kurruk72@gmail.com>
2023-03-21 19:53:56 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Ivan", "Ivanov", (byte) 34);
+        us.saveUser("Alex", "Alexandrov", (byte) 43);
+        us.saveUser("Petr", "Petrov", (byte) 33);
+        us.saveUser("Oleg", "Olegov", (byte) 23);
+        for (User user : us.getAllUsers()) {
+            System.out.println(user);
+        }
+        us.cleanUsersTable();
+        us.dropUsersTable();
     }
 }
